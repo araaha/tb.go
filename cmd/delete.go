32ed5df882fedd53d4178c89f34b2044dfc967d6
araaha <ara.ahady@gmail.com>
2024-07-15 00:00:25 -0400

    Added cmd/delete

---
+++ cmd/delete.go
@@ -0,0 +1,55 @@
+/*
+Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/spf13/cobra"
+)
+
+// deleteCmd represents the delete command
+var deleteCmd = &cobra.Command{
+	Use:   "delete",
+	Short: "Delete item",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		if len(args) > 0 {
+			for _, id := range args {
+				id, err := strconv.Atoi(id)
+				if err != nil {
+					fmt.Printf("Unable to find item with id: %d", id)
+				} else {
+					taskBook.Delete(id)
+					taskBook.Store("/home/araaha/taskbook.json")
+					fmt.Printf("Deleted item: %d", id)
+				}
+			}
+		}
+	},
+	ValidArgsFunction: func(cmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {
+		ids := taskBook.AllID(false)
+		return ids, cobra.ShellCompDirectiveNoFileComp
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(deleteCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// deleteCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// deleteCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
