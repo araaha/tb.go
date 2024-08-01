448d869dc9226864a36d7799daee45683b9111a4
araaha <ara.ahady@gmail.com>
2024-08-01 00:03:57 -0400

    Added config file (via viper) and refactored root.go

--- cmd/root.go
+++ cmd/root.go
@@ -1,29 +1,46 @@
 package cmd
 
 import (
-	"github.com/araaha/tb/taskbook"
-	"github.com/spf13/cobra"
+	"fmt"
 	"os"
+	"path/filepath"
+
+	"github.com/araaha/tb.go/taskbook"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
 var (
+	ver      bool
+	Version  string
+	cfgFile  string
 	taskBook tb.Book
-	rootCmd  = &cobra.Command{
-		Use:   "tb",
-		Short: "A Taskbook",
-		// Uncomment the following line if your bare application
-		// has an action associated with it:
-		PersistentPreRun: func(cmd *cobra.Command, args []string) {
-			taskBook.Read("/home/araaha/taskbook.json")
-		},
-		Run: func(cmd *cobra.Command, args []string) {
-			taskBook.DisplayByBoard()
-		},
-		PersistentPostRun: func(cmd *cobra.Command, args []string) {
-			taskBook.Store("/home/araaha/taskbook.json")
-		},
-	}
 )
+
+// rootCmd displays board or version. It reads the data and stores it.
+var rootCmd = &cobra.Command{
+	Use:   "tb",
+	Short: "A Taskbook",
+	PersistentPreRun: func(cmd *cobra.Command, args []string) {
+		if err := taskBook.Read(); err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+	},
+	Run: func(_ *cobra.Command, _ []string) {
+		if ver {
+			fmt.Println(Version)
+			return
+		}
+		taskBook.DisplayByBoard()
+	},
+	PersistentPostRun: func(cmd *cobra.Command, args []string) {
+		if err := taskBook.Store(); err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+	},
+}
 
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
@@ -31,17 +48,54 @@ func Execute() {
 	if err := rootCmd.Execute(); err != nil {
 		os.Exit(1)
 	}
-	tb.Create()
 }
 
 func init() {
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
+	cobra.OnInitialize(initConfig)
 
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.taskbook.go.yaml)")
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $XDG_CONFIG_HOME/taskbook/taskbook.toml)")
+	rootCmd.PersistentFlags().BoolVarP(&ver, "version", "v", false, "Display current version")
+	_ = rootCmd.RegisterFlagCompletionFunc("config", func(_ *cobra.Command, _ []string, _ string) ([]string, cobra.ShellCompDirective) {
+		return []string{"toml"}, cobra.ShellCompDirectiveFilterFileExt
+	})
 
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	// rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+func initConfig() {
+	if cfgFile != "" {
+		// Use config file from the flag.
+		viper.SetConfigFile(cfgFile)
+	} else {
+		// Find home directory.
+		home, err := os.UserHomeDir()
+		cobra.CheckErr(err)
+
+		var cfgPath string
+
+		xdgConfigHome := os.Getenv("XDG_CONFIG_HOME")
+
+		if xdgConfigHome != "" {
+			cfgPath = filepath.Join(xdgConfigHome, "taskbook")
+		} else {
+			cfgPath = filepath.Join(home, ".config", "taskbook")
+		}
+
+		// Search config in XDG first, followed by .config/taskbook
+		viper.AddConfigPath(cfgPath)
+		viper.SetConfigName("taskbook")
+		viper.SetConfigType("toml")
+
+		if err := viper.ReadInConfig(); err != nil {
+			if _, ok := err.(viper.ConfigFileNotFoundError); ok {
+				if err := viper.SafeWriteConfig(); err != nil {
+					fmt.Println(err)
+					return
+				}
+			} else {
+				fmt.Println(err)
+				return
+			}
+		}
+
+	}
 }
