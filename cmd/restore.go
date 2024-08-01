269829a618d7ff2e68f5b4e68b1f422c4f7fce35
araaha <ara.ahady@gmail.com>
2024-08-01 00:03:07 -0400

    Added cmd/restore.go

---
+++ cmd/restore.go
@@ -0,0 +1,45 @@
+package cmd
+
+import (
+	"fmt"
+	"strconv"
+
+	tb "github.com/araaha/tb.go/taskbook"
+	"github.com/spf13/cobra"
+)
+
+// restoreCmd represents the restore command
+var restoreCmd = &cobra.Command{
+	Use:   "restore",
+	Short: "Restore items from archive",
+	Run:   restore,
+	ValidArgsFunction: func(cmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {
+		return taskBook.GetAllID(false), cobra.ShellCompDirectiveNoFileComp
+	},
+}
+
+// restore restores archived items
+func restore(_ *cobra.Command, args []string) {
+	var ids []string
+
+	if err := validArgs(args, false); err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	for _, arg := range args {
+		id, _ := strconv.Atoi(arg)
+		taskBook.Update(id, func(item tb.Item) tb.Item {
+			item.GetBaseItem().IsArchive = !item.GetBaseItem().IsArchive
+			return item
+		})
+
+		ids = append(ids, arg)
+	}
+
+	fmt.Println(tb.MarkRestored(ids))
+}
+
+func init() {
+	rootCmd.AddCommand(restoreCmd)
+}
